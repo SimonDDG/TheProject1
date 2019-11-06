7e05a4ec3cb8407be1f3d2409242efaeb11c8e54
SimonDDG <simon.douchka@gmail.com>
2019-11-06 11:53:02 +0100

    Skapar min branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hoppas det funkar");
+        System.out.println("hoppas det funkar igen");
     }
 }
