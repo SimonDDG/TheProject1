d122223537cf06982231956f139ff5bc0484f3c4
slothexpress <sami890905@gmail.com>
2019-11-06 12:55:36 +0100

    Added branch: SamiSami och print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-<<<<<<< HEAD
+
         System.out.println("hoppas det funkar");
 
         System.out.println("TJENARE");
         System.out.println("deleted ANOTHER BRANCH");
-System.out.println("Hejsan");
+
+        System.out.println("Hejsan");
         System.out.println("Detta är Robins första rad kod");
+
         System.out.println("hoppas det funkar igen");
         System.out.println("Simon lägger till en grej till");
+
+
+        System.out.println("Branch: SamiSami");
     }
 }
